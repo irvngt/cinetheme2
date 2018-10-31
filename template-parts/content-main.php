372639d58713b5d1737828a833edf26cd5817351
irving negrete <irving@webdesignrs.com>
2018-10-30 22:55:59 -0600

    style.scss importa & reset.scss

--- template-parts/content-main.php
+++ template-parts/content-main.php
@@ -5,9 +5,6 @@
     <a href="<?php the_permalink();?>"><?php the_title();?></a>
   </h2> 
   <?php the_excerpt(); ?>
-  <br>
-  <?php  
-    the_author_posts_link();
-  ?>
+  <?php get_template_part('template-parts/content-single'); ?>
 </figcaption>
 </figure>
